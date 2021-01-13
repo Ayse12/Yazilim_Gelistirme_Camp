3ca4d6b6d2aaa4328e50270abca6de6631e03536
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-13 22:45:32 +0300

    Create degiskenler.py

---
+++ degiskenler.py
@@ -0,0 +1,14 @@
+baslik = 'İlginizi çekebilir!!!' #string
+vade = 48 #integer
+faizOrani = 1.69 #float
+
+print('Başlık : ' + baslik)
+print(vade + vade)
+print(faizOrani)
+
+print(type(faizOrani))
+print(type(vade))
+print(type(baslik))
+
+#type = veri tipi
+#operator
