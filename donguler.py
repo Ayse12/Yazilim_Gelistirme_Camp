e7d8fda823f497542b4668ee33560e45abc0fcf1
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-13 22:45:11 +0300

    Create donguler.py

---
+++ donguler.py
@@ -0,0 +1,14 @@
+kredi1 = "Hızlı Kredi"
+kredi2 = "Maaşını Halkbanktan..."
+kredi3 = "Mutlu emekli"
+kredi4 = "Özel Sektör"
+
+print(kredi1)
+print(kredi2)
+print(kredi3)
+print(kredi4)
+
+krediler = ["Hızlı Kredi","Maaşını Halkbanktan...","Mutlu emekli","Özel Sektör"]
+
+for kredi in krediler:
+  print(kredi)
