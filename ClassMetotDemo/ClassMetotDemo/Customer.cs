9b965e4e03eae4d1d00f2598c9e00fe29fbcaa50
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-17 16:06:45 +0300

    Ödev 3

---
+++ ClassMetotDemo/ClassMetotDemo/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Customer
+    {
+        public int CustomerId { get; set; }
+        public string NameAndSurname { get; set; }
+    }
+}
