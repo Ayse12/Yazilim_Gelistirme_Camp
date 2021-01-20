a8f70cd3c9568ef8145675c2b7698f661451790e
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-21 00:29:18 +0300

    3_Odev
    3. Gun sonunda eğitimi pekiştirmek için verilen ödev kodları

---
+++ 3_Odev/3_Odev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ref te number1 bir değer atamış olmak gerekiyor.out ta ise localde tanımlamak yeterli oluyor. Ekstra değer atamıyoruz.
+            int number1 = 20;
+            int number2 = 100;
+            var result = Add(ref number1, number2);
+            Console.WriteLine(result);
+            Console.WriteLine(number1);
+        }
+        static int  Add(ref int number1,int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+    }
+}
