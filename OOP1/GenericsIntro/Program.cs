f7cd2e282b8ce67f543d51bd84c34aa9748d6ef8
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-21 00:08:44 +0300

    4. Gun Ders
    Derste işlenen kodların olduğu klasör eklenildi.

---
+++ OOP1/GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+            //List<string> isimler2 = new List<string>();
+            //Console.WriteLine(isimler2.Count);
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Kerem");
+
+            Console.WriteLine(isimler.Length);
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
