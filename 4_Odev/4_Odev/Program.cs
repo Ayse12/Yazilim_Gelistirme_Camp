6ab3e1f7007634552adeb53ad4a3e07132d81023
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-21 14:24:00 +0300

    Dictionary Project
    Benim yazdığım kendi My List classım ile metot sayesinde listeye eleman ekleme işleminin yapıldığı kodlar yazıldı.

---
+++ 4_Odev/4_Odev/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DictionaryProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> meyveler = new MyDictionary<string>();
+            meyveler.Add("Portakal");
+            meyveler.Add("Elma");
+            meyveler.Add("Armut");
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Adet : " + meyveler.Count());
+            meyveler.Add("Nar");
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Adet : " + meyveler.Count());
+            Console.WriteLine("-------------");
+
+            foreach (var meyve in meyveler.meyvelerim)
+            {
+                Console.WriteLine(meyve);
+            }
+
+            Console.WriteLine("-------------");
+        }
+    }
+}
+
