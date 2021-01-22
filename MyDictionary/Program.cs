8a7e39db25b909f0202957a4c91fa0f8a2e1f36c
Ayşe İlhanlı <54233566+Ayse12@users.noreply.github.com>
2021-01-22 17:20:47 +0300

    4.Gun Odev 5

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> meyveler = new MyDictionary<string, string>();
+            meyveler.Add("Elma", "Yeşil");
+            meyveler.Add("Armut", "Sarı");
+            meyveler.Add("Nar", "Kırmızı");
+            meyveler.Add("Üzüm", "Siyah");
+            Console.WriteLine(meyveler.Length);
+
+            foreach (var ismi in meyveler.Keys)
+            {
+                Console.WriteLine(ismi);
+            }
+
+            foreach (var isim in meyveler.Values)
+            {
+                Console.WriteLine(isim);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
