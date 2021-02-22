09abd290e28ac7bd07dfa62f1dc33ab85a2760a4
AYŞE İLHANLI <20162013027@std.nisantasi.edu.tr>
2021-02-22 13:53:27 +0300

    10. Gün C# Kurumsal Yazılım Mimarileri  1 Wep API Kodlamaya Giris

--- FinalProject/Business/Concrete/ProductManager.cs
+++ FinalProject/Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
@@ -11,32 +12,44 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         //Bir iş sınıfı başka sınıfları newlemez.Bu yüzden böyle çağırırız.
-        IProductDal _ProductDal;
+        IProductDal _productDal;
 
         public ProductManager(IProductDal productDal)
         {
-            _ProductDal = productDal;
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product product)
+        {
+            //business Codes
+            _productDal.Add(product);
+            return new Result(true,"Ürün Eklendi");
         }
 
         public List<Product> GetAll()
         {
             //İş Kodları
-            return _ProductDal.GetAll();
+            return _productDal.GetAll();
         }
 
         public List<Product> GetAllCategoryId(int id)
         {
-            return _ProductDal.GetAll(p => p.CategoryId == id);
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
         {
-            return _ProductDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
 
         public List<ProductDetailDto> GetProductDetails()
         {
-            return _ProductDal.GetProductDetails();
+            return _productDal.GetProductDetails();
         }
     }
 }
---
+++ FinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //Geter (get) readonly dir ve constructerda set edilebilirler.
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
